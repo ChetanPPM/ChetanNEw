8544e8fe1351be435ebcad6544bc14d63e71186e
POOJA <POOJA@DESKTOP-HKGAK7I>
2024-01-03 21:39:59 +0530

    Third Changes

--- Demo/src/sample/Program1.java
+++ Demo/src/sample/Program1.java
@@ -2,9 +2,13 @@ package sample;
 
 public class Program1 
 {
+	public void name() {
+		
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("GitHub Demo");
 		System.out.println("Hi");
+		
 	}
 }
